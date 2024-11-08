fba9a4e51bb079c4fe196f28ea844efa617df362
Beerkanwar <beer14246@gmail.com>
2024-11-08 10:01:27 +0530

    Updated student

---
+++ Beer's-Files/main.cpp
@@ -0,0 +1,13 @@
+#include"Project.cpp"
+#include"Attendance.cpp"
+#include"Teacher.cpp"
+#include"Students.cpp"
+
+main(){
+    int N;
+    cin>>N;
+    switch(N){
+        case 1:
+    }
+    return 0;
+}
